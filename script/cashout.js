67817b9c90fe37e21bd4bc00b8147a0e66da5c51
Redwanhossain200 <mdredwanhossain2007@gmail.com>
2026-02-18 12:38:42 +0600

    add cashout

---
+++ script/cashout.js
@@ -0,0 +1,28 @@
+// console.log();
+
+document.getElementById("cashout-btn").addEventListener("click", function () {
+  // 1- get the agent number & validate
+  const cashoutNumberInput = document.getElementById("cashout-number");
+  const cashoutNumber = cashoutNumberInput.value;
+  console.log(cashoutNumber);
+
+  // 2- get the amount, validate, convert to number
+  const cashoutAmountInput = document.getElementById("cashout-amount");
+  const cashoutAmount = cashoutAmountInput.value;
+  console.log(cashoutAmount);
+
+  // 3- get the current balance
+  const balaceElement = document.getElementById("balance");
+  const balance = balaceElement.innerText;
+  console.log(balance);
+
+
+  // 4- Calculate new balance
+
+  // 5- get the pin and verify
+
+  // 5-1 true::show an alert > set balance
+
+  // 5-2 true::show an error alert > return
+
+});
